711636614489fae5fc4e1ad17c0f823232e33938
weibin yu <wy222ad@student.lnu.se>
2020-11-16 17:18:39 +0100

    add .gitignore

--- simple-react-recruitment-app/server/routes/index.js
+++ simple-react-recruitment-app/server/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/register',(res,req)=>{
+  registerNewUserIfNotExist(res,req)
+})
+
 module.exports = router;
+
+function registerNewUserIfNotExist(res,req){
+
+}
