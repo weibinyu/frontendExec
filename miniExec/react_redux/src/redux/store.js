4b5b1c593b3d277dc7cbcf87205340e5c42090bf
weibin yu <wy222ad@student.lnu.se>
2020-12-04 14:30:32 +0100

    remove uneccessary files

--- miniExec/react_redux/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import {createStore,applyMiddleware} from 'redux'
-import {counter} from "./reducers"
-import thunk from "redux-thunk";
-
-const store = createStore(
-    counter,
-    applyMiddleware(thunk)
-)
-export default store
