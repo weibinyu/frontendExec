43b77a92efac61821887d5d5d551b98a96dc3940
weibin yu <wy222ad@student.lnu.se>
2020-12-05 16:30:53 +0100

    encapsulate a simple navbar, currently only used in Main view

--- vue/mall/src/views/main/Main.vue
+++ vue/mall/src/views/main/Main.vue
@@ -1,13 +1,25 @@
 <template>
-  <h2>Main</h2>
+  <div id="main" class="home-nav">
+    <nav-bar>
+      <div slot="center">Shopping mall</div>
+    </nav-bar>
+  </div>
+
 </template>
 
 <script>
-export default {
-  name: "Main"
-}
+  import NavBar from "@/components/common/navbar/NavBar";
+  export default {
+    name: "Main",
+    components:{
+      NavBar
+    }
+  }
 </script>
 
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+  }
 </style>
