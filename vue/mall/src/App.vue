46efe055969bc309b99ba67ac3b72ecdd1b90bd3
weibin yu <wy222ad@student.lnu.se>
2020-12-05 14:50:52 +0100

    import tab bar icons, create tab bar and tab bar item components, finish static tab bar

--- vue/mall/src/App.vue
+++ vue/mall/src/App.vue
@@ -1,13 +1,34 @@
 <template>
   <div id="app">
+    <tab-bar>
+      <tab-bar-item>
+        <img slot="item-icon" src="~assets/img/tabbar/home.svg"/>
+        <div slot="item-text">Main</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="~assets/img/tabbar/category.svg"/>
+        <div slot="item-text">Category</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="~assets/img/tabbar/shopcart.svg"/>
+        <div slot="item-text">Cart</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="~assets/img/tabbar/profile.svg"/>
+        <div slot="item-text">Profile</div>
+      </tab-bar-item>
+    </tab-bar>
   </div>
 </template>
 
 <script>
-
+  import TabBar from "@/components/common/tabbar/TabBar";
+  import TabBarItem from "@/components/common/tabbar/TabBarItem";
 export default {
   name: 'App',
   components: {
+    TabBar,
+    TabBarItem
   }
 }
 </script>
