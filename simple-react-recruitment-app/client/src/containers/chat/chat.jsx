565eddeb04fe15d64e1b2e6caed465167640331d
weibin yu <wy222ad@student.lnu.se>
2020-12-18 13:15:29 +0100

    create static view for chat component. PS: Forgot to mention routes created and changed user-list for chat

--- simple-react-recruitment-app/client/src/containers/chat/chat.jsx
+++ simple-react-recruitment-app/client/src/containers/chat/chat.jsx
@@ -1,11 +1,33 @@
-import React,{useState} from 'react'
+import React from 'react'
 import {connect} from 'react-redux'
+import {InputItem, List, NavBar} from "antd-mobile";
+
+const Item = List.Item
 
 function Chat(props){
   return (
-      <>
-        Chat
-      </>
+      <div id='chat-page'>
+        <NavBar>aa</NavBar>
+        <List>
+          <Item
+              thumb={require('@/assets/avatars/Avatar1.png').default}
+          >
+            Hi
+          </Item>
+          <Item className='chat-me' extra='me'>
+            Hi
+          </Item>
+        </List>
+
+        <div className='am-tab-bar'>
+          <InputItem
+              placeholder="input bar"
+              extra={
+                <span>Send</span>
+              }
+          />
+        </div>
+      </div>
   )
 }
 
